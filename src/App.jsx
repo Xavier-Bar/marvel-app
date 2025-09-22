aecd67725bff45facb4c9776e080e5a7b55b7046
Xavier <xavier.barbeau@etu.univ-poitiers.fr>
2025-09-22 18:21:53 +0200

    Create NumberOfCharacters component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,13 @@
 import './App.css'
 import characters from './data/characters.json';
 import CharactersList from './components/CharactersList';
-
-function CharactersCount({ characters }) {
-  const count = characters.length;
-  
-  return (
-    <p>Nombre de personnages : {count}</p>
-  );
-}
+import NumberOfCharacters from './components/NumberOfCharacters';
 
 function App() {
   return (
     <div>
       <h1>Marvel App</h1>
-      <CharactersCount characters={characters} />
+      <NumberOfCharacters characters={characters} />
       <CharactersList characters={characters} />
     </div>
   );
---
+++ src/components/NumberOfCharacters.jsx
@@ -0,0 +1,11 @@
+function NumberOfCharacters({ characters }) {
+  const count = characters.length;
+  
+  if (count === 0) {
+    return <p>There is no character</p>;
+  }
+  
+  return <p>There is {count} characters</p>;
+}
+
+export default NumberOfCharacters;
